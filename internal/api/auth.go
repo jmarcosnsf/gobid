4fecc1e66157b3e81fb5763e18614d0bd592086f
jmnsf <jmarcosnsf@gmail.com>
2026-05-08 17:12:58 -0300

    feat: add CSRF protection with gorilla/csrf

--- internal/api/auth.go
+++ internal/api/auth.go
@@ -3,8 +3,13 @@ package api
 import (
 	"net/http"
 
+	"github.com/gorilla/csrf"
 	"github.com/jmarcosnsf/gobid/internal/jsonutils"
 )
+func (api *Api) HandleGetCSRFtoken(w http.ResponseWriter, r *http.Request){
+	token := csrf.Token(r)
+	jsonutils.EncondeJson(w, r, http.StatusOK, map[string]any{"csrf_token": token})
+}
 
 func (api *Api) AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request){
