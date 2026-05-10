7e67aa30928d8c409938cf7f3c358ed7ae0af368
jmnsf <jmarcosnsf@gmail.com>
2026-05-10 17:06:46 -0300

    feat: implement bid service

--- internal/store/pgstore/queries/products.sql
+++ internal/store/pgstore/queries/products.sql
@@ -3,3 +3,8 @@ INSERT INTO products(
   seller_id, product_name, description, baseprice, auction_end
 ) VALUES($1, $2, $3, $4, $5)
 RETURNING id;
+
+-- name: GetProductById :one
+SELECT * FROM products
+WHERE id = $1;
+
